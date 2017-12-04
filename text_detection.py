1770a960c6ed9ee7dc091c510218d28e9d11d31d
stardust <ranjun4366@gmail.com>
2017-12-04 15:46:06 +0800

    update text_detection.py

--- text_detection.py
+++ text_detection.py
@@ -3,16 +3,26 @@
 # opencv 3.2.0
 import cv2
 import numpy as np
+import nms
 
-img = cv2.imread('test.jpg')
-mser = cv2.MSER_create()
+img = cv2.imread('test2.jpg')
+mser = cv2.MSER_create(_max_area=300)
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 regions, boxes = mser.detectRegions(gray)
+hulls = [cv2.convexHull(p.reshape(-1, 1, 2)) for p in regions]
 
-for box in boxes:
-    x, y, w, h = box
-    cv2.rectangle(img, (x,y),(x+w, y+h), (255, 0, 0), 1)
+keep = []
+for box in hulls:
+    x, y, w, h = cv2.boundingRect(box)
+    # cv2.rectangle(img, (x,y),(x+w, y+h), (255, 0, 0), 1)
+    keep.append([x, y, x + w, y + h])
+
+keep2=np.array(keep)
+pick = nms.nms(keep2, 0.1)
+
+for (startX, startY, endX, endY) in pick:
+    cv2.rectangle(img, (startX, startY), (endX, endY), (255, 0, 0), 1)
 
 cv2.imshow('test', img)
 cv2.waitKey(0)
