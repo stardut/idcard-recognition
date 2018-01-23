8cc3318ef59e027489283b36f9ae421653f6953b
stardust <ranjun4366@gmail.com>
2018-01-23 13:48:52 +0800

    fix bugs

--- rnn/lstm.py
+++ rnn/lstm.py
@@ -7,49 +7,46 @@ import numpy as np
 
 class LSTM_CTC(object):
     """docstring for LSTM"""
-    def __init__(self, num_layer, num_units, num_class,
-                input_size, batch_size, time_step, learn_rate):
+    def __init__(self, num_layer, num_units, num_class, input_size, batch_size):
         self.num_layer = num_layer
         self.num_units = num_units
         self.num_class = num_class
-        self.time_step = time_step
         self.input_size = input_size
         self.batch_size = batch_size
-        self.learn_rate = learn_rate
 
     def build(self):
-        with tf.Graph.device('/gpu:1'):
-            self.global_step = tf.Variable(0, trainable=False)
-            self.X = tf.placeholder(dtype=tf.float32, shape=[None, None, self.input_size], name='input')
-            self.labels = tf.sparse_placeholder(tf.int32, name='label')
-            self.seq_len = tf.placeholder(tf.int32, [None], name='sequence_len')    
-            self.keep_prob = tf.placeholder(tf.float32, name='keep_prob')
+        self.global_step = tf.Variable(0, trainable=False)
+        self.learn_rate = tf.placeholder(tf.float32, name='learn_rate')
+        self.X = tf.placeholder(dtype=tf.float32, shape=[None, None, self.input_size], name='input')
+        self.labels = tf.sparse_placeholder(tf.int32, name='label')
+        self.seq_len = tf.placeholder(tf.int32, [None], name='sequence_len')    
+        self.keep_prob = tf.placeholder(tf.float32, name='keep_prob')
 
 
-            mlstm_cell = tf.nn.rnn_cell.MultiRNNCell([self.unit() for i in range(self.num_layer)])
-            self.init_state = mlstm_cell.zero_state(self.batch_size, dtype=tf.float32)
-            outputs, state = tf.nn.dynamic_rnn(cell=mlstm_cell, 
-                                               inputs=self.X,
-                                               sequence_length=self.seq_len,
-                                               initial_state=self.init_state,
-                                               time_major=False)
+        mlstm_cell = tf.nn.rnn_cell.MultiRNNCell([self.unit() for i in range(self.num_layer)])
+        self.init_state = mlstm_cell.zero_state(self.batch_size, dtype=tf.float32)
+        outputs, state = tf.nn.dynamic_rnn(cell=mlstm_cell, 
+                                           inputs=self.X,
+                                           sequence_length=self.seq_len,
+                                           initial_state=self.init_state,
+                                           time_major=False)
 
-            outputs = tf.reshape(outputs, [-1, self.num_units])
-            W = tf.Variable(tf.truncated_normal([self.num_units, self.num_class], stddev=0.1), 
-                dtype=tf.float32)
-            b = tf.Variable(tf.constant(0.1, shape=[self.num_class], dtype=tf.float32))
+        outputs = tf.reshape(outputs, [-1, self.num_units])
+        W = tf.Variable(tf.truncated_normal([self.num_units, self.num_class], stddev=0.1), 
+            dtype=tf.float32)
+        b = tf.Variable(tf.constant(0.1, shape=[self.num_class], dtype=tf.float32))
 
-            logits = tf.matmul(outputs, W) + b
-            logits = tf.reshape(logits, [self.batch_size, -1, self.num_class])
-            logits = tf.transpose(logits, (1, 0, 2))
+        logits = tf.matmul(outputs, W) + b
+        logits = tf.reshape(logits, [self.batch_size, -1, self.num_class])
+        logits = tf.transpose(logits, (1, 0, 2))
 
+        self.loss = tf.reduce_mean(
+            tf.nn.ctc_loss(labels=self.labels, inputs=logits, sequence_length=self.seq_len))
+        self.train_op = tf.train.AdamOptimizer(self.learn_rate).minimize(self.loss, self.global_step)
 
-            self.loss = tf.reduce_mean(
-                tf.nn.ctc_loss(labels=self.labels, inputs=logits, sequence_length=self.seq_len))
-            self.train_op = tf.train.AdamOptimizer(self.learn_rate).minimize(self.loss, self.global_step)
-
-            self.decoded, log_prob = tf.nn.ctc_beam_search_decoder(logits, self.seq_len, merge_repeated=False)    
-            self.acc = tf.reduce_mean(tf.edit_distance(tf.cast(self.decoded[0], tf.int32), self.labels))
+        self.decoded, log_prob = tf.nn.ctc_greedy_decoder(logits, seq_len,merge_repeated=False)
+        # self.decoded, log_prob = tf.nn.ctc_beam_search_decoder(logits, self.seq_len, merge_repeated=False)    
+        self.acc = tf.reduce_mean(tf.edit_distance(tf.cast(self.decoded[0], tf.int32), self.labels))
 
 
     def unit(self):
--- rnn/train.py
+++ rnn/train.py
@@ -14,18 +14,17 @@ model_path = 'model'
 if not os.path.exists(model_path):
     os.mkdir(model_path)
 
-img_shape = (28, 256)
+img_shape = (32, 256)
 data = Data(img_shape)
 
 num_layer = 2
-num_units = 512
+num_units = 256
 num_class = data.word_dict.word_num + 1 # 1: ctc_blank
 keep_prob = 0.5
 input_size = img_shape[0]
-time_step = img_shape[1]
 
 learn_rate = 0.01
-batch_size = 32
+batch_size = 64
 step = 10000 * 100
 
 
@@ -34,7 +33,6 @@ model = LSTM_CTC(num_layer=num_layer,
                  num_class=num_class,
                  input_size=input_size,
                  batch_size=batch_size,
-                 learn_rate=learn_rate,
                  time_step=time_step)
 
 model.build()
@@ -45,10 +43,8 @@ with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
     saver = tf.train.Saver(tf.global_variables(), max_to_keep=5)
     start = time.time()
     for i in range(step):
-        st1 = time.time()
         inputs, labels, seq_len = data.get_batch(batch_size)
-        st2 = time.time()
-        # seq_len = np.ones(batch_size) * img_shape[1]
+        seq_len = np.ones(batch_size) * img_shape[1]
 
         feed = {
             model.X : inputs,
@@ -58,19 +54,18 @@ with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
         }
         decoded, loss, _ = sess.run([model.decoded, model.loss, model.train_op], feed_dict=feed)
 
-        st3 = time.time()
-        # if i % 100 == 0:
-        pre = data.decode_sparse_tensor(decoded[0])
-        ori = data.decode_sparse_tensor(labels)
-        acc = data.hit(pre, ori)
-        t = (time.time() - start) / (i+1)
-        st4 = time.time()
-        print('step: {}, accuracy: {:.4f}, loss: {:.6f}, time cost per step: {:.3f}'.format(i, acc, loss, t))
-        print('origin : ' + ori[0])
-        print('predict: ' + pre[0])
-        print('gen data: {:.3f}, run: {:.3f}, decode: {:.3f}'.format(st2-st1, st3-st2, st4-st3))
+        if (i+1) % 100 == 0:
+            pre = data.decode_sparse_tensor(decoded[0])
+            ori = data.decode_sparse_tensor(labels)
+            acc = data.hit(pre, ori)
+            t = (time.time() - start) / (i+1)
+            print('step: {}, accuracy: {:.4f}, loss: {:.6f}, speed: {:.3f}s / step, lr: {}' \
+                .format(i, acc, loss, t, learn_rate))
+            print('origin : ' + ori[0])
+            print('predict: ' + pre[0])
+            learn_rate = learn_rate * 0.92 ** (i / 1000)
 
-        if i % 10000 == 0:
+        if (i+1) % 3000 == 0:
             checkpoint_path = os.path.join(model_path, 'model.ckpt')
             saver.save(sess, checkpoint_path, global_step=i)
             print('save model in step: {}'.format(i))
